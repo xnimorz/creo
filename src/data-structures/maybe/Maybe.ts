95a2a8e6aeaf2fea5ca1d4e2177997ed12e85db9
Nik <nik@xnim.me>
2025-08-16 20:20:11 +0100

    Simplify maybe

--- src/data-structures/maybe/Maybe.ts
+++ src/data-structures/maybe/Maybe.ts
@@ -2,23 +2,15 @@ export type None = null | undefined;
 export type Just<T> = T;
 export type Maybe<T> = Just<T> | None;
 
-export function isJust<T>(v: Maybe<T>): v is Just<T> {
-  return v != null;
-}
-
-export function isNone<T>(v: Maybe<T>): v is None {
-  return v == null;
-}
-
 export function unwrap<T>(v: Maybe<T>): Just<T> {
-  if (isJust(v)) {
+  if (v != null) {
     return v;
   }
   throw new TypeError("Optional is none");
 }
 
-export function orDefault<T, K>(v: Maybe<T>, alternative: K) {
-  if (isJust(v)) {
+export function withDefault<T, K>(v: Maybe<T>, alternative: K) {
+  if (v != null) {
     return v;
   }
   return alternative;
